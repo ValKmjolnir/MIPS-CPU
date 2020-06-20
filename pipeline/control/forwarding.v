fa6d952df8afc2735d2641512e2f710a05f5b7a6
Valk Richard Li <48872266+ValKmjolnir@users.noreply.github.com>
2020-06-20 08:57:30 -0700

    update

--- pipeline/control/forwarding.v
+++ pipeline/control/forwarding.v
@@ -10,8 +10,15 @@ module forwarding
 );
 
 input wire[43:0] ifid_reg;
+wire[5:0] ifid_op;
 input wire[159:0] idex_reg;
+wire[5:0] idex_op;
 input wire[127:0] memwr_reg;
+wire[5:0] memwr_op;
 output wire BusAchange,BusBchange,ALUinAchange,ALUinBchange;
+
+assign ifid_op=ifid_reg[31:26];
+assign idex_op=idex_reg[31:26];
+assign memwr_op=memwr_reg[31:26];
 
 endmodule
