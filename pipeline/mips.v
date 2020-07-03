216191f458efa8e494fd192be99d463426034948
Valk Richard Li <48872266+ValKmjolnir@users.noreply.github.com>
2020-07-03 00:53:24 -0700

    update

--- pipeline/mips.v
+++ pipeline/mips.v
@@ -5,14 +5,12 @@ reg clk,reset;
 cu mips_cu(clk,reset);
 
 initial begin
-    #2 clk   <=1;
-    #2 clk   <=0;
-    reset <=1;
+    #4 reset <=1;
 end
 
 always begin
-    #2 clk <= 1;
     #2 clk <= 0;
+    #2 clk <= 1;
 end
 
 endmodule
