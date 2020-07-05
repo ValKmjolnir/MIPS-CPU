62b8459901d6518401bde538157b453beb2b9a1d
Valk Richard Li <48872266+ValKmjolnir@users.noreply.github.com>
2020-07-05 03:54:47 -0700

    update

--- pipeline45/datapath/dm.v
+++ pipeline45/datapath/dm.v
@@ -6,35 +6,24 @@ module dm
     BusW,
     BusR,
     ByteWidth,
-    DmSignExt,
-    DmError
+    DmSignExt
 );
 
 input wire       clk,wEn,DmSignExt;
 input wire[31:0] addr,BusW;
 input wire[1:0]  ByteWidth;
-output reg       DmError;
 output reg[31:0] BusR;
 reg[31:0]        memory[0:1023];// use little-endian
 integer          i;
 
 initial begin
     for(i=0;i<1024;i=i+1) memory[i]<=0;
-    BusR    <= 0;
-    DmError <= 0;
+    BusR <= 0;
 end
 
 always@(*) begin
     if(ByteWidth==2'b11) begin// 4 bytes
-        DmError <= (addr[1:0]>2'd0);
         BusR <= memory[addr>>2];
-    end
-    else if(ByteWidth==2'b10) begin// 2 bytes
-        DmError <= (addr[0]!=0);
-        case (addr[0])
-            0:BusR <= {DmSignExt?(memory[addr>>2][15]? 16'hffff:16'h0000):16'h0000,memory[addr>>2][15:0] };
-            1:BusR <= {DmSignExt?(memory[addr>>2][31]? 16'hffff:16'h0000):16'h0000,memory[addr>>2][31:16]};
-        endcase
     end
     else if(ByteWidth==2'b01) begin// 1 byte
         case (addr[1:0])
@@ -44,20 +33,14 @@ always@(*) begin
             2'b11:BusR <= {DmSignExt?(memory[addr>>2][31]? 24'hffffff:24'h000000):24'h000000,memory[addr>>2][31:24]};
         endcase
     end
+    else
+        BusR <= 32'd0;
 end
 
 always@(posedge clk) begin
     if(wEn) begin
         if(ByteWidth==2'b11) begin// 4 bytes
-            DmError <= (addr[1:0]>2'd0);
             memory[addr>>2] <= BusW;
-        end
-        else if(ByteWidth==2'b10) begin// 2 bytes
-            DmError <= (addr[0]!=0);
-            case (addr[0])
-                0:memory[addr>>2][15:0]  <= BusW[15:0];
-                1:memory[addr>>2][31:16] <= BusW[15:0];
-            endcase
         end
         else if(ByteWidth==2'b01) begin// 1 byte
             case (addr[1:0])
@@ -67,6 +50,8 @@ always@(posedge clk) begin
                 2'b11:memory[addr>>2][31:24] <= BusW[7:0];
             endcase
         end
+        else
+            memory[addr>>2] <= 32'd0;
     end
 end
 endmodule
